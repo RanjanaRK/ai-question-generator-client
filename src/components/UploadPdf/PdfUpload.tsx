0d9f6a4e9930048183c978ee0b93bbfdbf3b594e
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-17 19:38:34 +0530

    add prettier

--- src/components/UploadPdf/PdfUpload.tsx
+++ src/components/UploadPdf/PdfUpload.tsx
@@ -22,23 +22,19 @@ const PdfUpload = () => {
         {/* Dropzone */}
         <div
           {...getRootProps()}
-          className="border-2 border-dashed border-foreground/45 rounded-xl
-                   bg-foreground/5 dark:bg-foreground/10
-                   flex flex-col items-center justify-center
-                   h-72 cursor-pointer transition
-                   w-full "
+          className="border-foreground/45 bg-foreground/5 dark:bg-foreground/10 flex h-72 w-full cursor-pointer flex-col items-center justify-center rounded-xl border-2 border-dashed transition"
         >
           <input {...getInputProps()} />
 
           <div className="text-center">
-            <div className="text-2xl font-semibold mb-2 border rounded-xl py-3 bg-foreground/15 hover:bg-foreground/25 w-64">
+            <div className="bg-foreground/15 hover:bg-foreground/25 mb-2 w-64 rounded-xl border py-3 text-2xl font-semibold">
               {isDragActive ? "Drop PDF here" : "Choose PDF file"}
             </div>
 
             <p className="opacity-80">or drag & drop files here</p>
 
             {file && (
-              <p className="mt-4 text-sm font-semibold px-3 py-1 rounded bg-accent">
+              <p className="bg-accent mt-4 rounded px-3 py-1 text-sm font-semibold">
                 {file.name}
               </p>
             )}
@@ -46,10 +42,7 @@ const PdfUpload = () => {
         </div>
 
         {/* Submit Button */}
-        <button
-          className="px-6 py-3 rounded-xl bg-primary text-primary-foreground
-                   hover:opacity-90 disabled:opacity-50 transition"
-        >
+        <button className="bg-primary text-primary-foreground rounded-xl px-6 py-3 transition hover:opacity-90 disabled:opacity-50">
           Generate questions
         </button>
       </div>
