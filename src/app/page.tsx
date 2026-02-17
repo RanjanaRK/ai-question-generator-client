5995ab4808a721be3f68dccc33fa9b6d72d1c304
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-17 16:23:21 +0530

    file upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import PdfUpload from "@/components/UploadPdf/PdfUpload";
+
 const page = () => {
   return (
     <>
-      <div className="">main page</div>
+      <div className=" py-6">
+        <h1 className="text-3xl font-bold text-center pb-10">
+          AI Question Generator
+        </h1>
+        <PdfUpload />
+      </div>
     </>
   );
 };
---
+++ src/components/UploadPdf/PdfUpload.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+const PdfUpload = () => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setFile(acceptedFiles[0]);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: { "application/pdf": [".pdf"] },
+    multiple: false,
+  });
+
+  return (
+    <>
+      <div className="grid place-items-center gap-4">
+        {/* Dropzone */}
+        <div
+          {...getRootProps()}
+          className="border-2 border-dashed border-foreground/45 rounded-xl
+                   bg-foreground/5 dark:bg-foreground/10
+                   flex flex-col items-center justify-center
+                   h-72 cursor-pointer transition
+                   md:w-2xl lg:w-4xl p-10"
+        >
+          <input {...getInputProps()} />
+
+          <div className="text-center">
+            <div
+              className="text-2xl font-semibold mb-2 border rounded-xl py-3
+                          bg-foreground/15 hover:bg-foreground/25 w-64"
+            >
+              {isDragActive ? "Drop PDF here" : "Choose PDF file"}
+            </div>
+
+            <p className="opacity-80">or drag & drop files here</p>
+
+            {file && (
+              <p className="mt-4 text-sm font-semibold px-3 py-1 rounded bg-accent">
+                {file.name}
+              </p>
+            )}
+          </div>
+        </div>
+
+        {/* Submit Button */}
+        <button
+          className="px-6 py-3 rounded-xl bg-primary text-primary-foreground
+                   hover:opacity-90 disabled:opacity-50 transition"
+        >
+          Generate questions
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default PdfUpload;
