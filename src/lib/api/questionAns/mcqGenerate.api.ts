72b8c193e15def422cf04fb31633629f52c6c173
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-28 23:16:53 +0530

    Create mcqGenerate.api.ts

---
+++ src/lib/api/questionAns/mcqGenerate.api.ts
@@ -0,0 +1,16 @@
+import { api } from "@/lib/axios";
+import { UploadPdfResponse } from "@/lib/types";
+
+export const mcqGenerate = async (pdfId: string) => {
+  try {
+    const { data } = await api.post("api/generate/mcq", {
+      pdfId,
+    });
+
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
