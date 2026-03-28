d5976a693bad9bb731d6941d2a2dff6e04c339f5
RanjanaRK <ranjanakumari.me@gmail.com>
2026-03-28 22:53:26 +0530

    Update me.api.ts

--- src/lib/api/user/me.api.ts
+++ src/lib/api/user/me.api.ts
@@ -6,8 +6,6 @@ export const getMe = async (): Promise<AuthResponse> => {
   try {
     const { data } = await api.get("api/user/me");
 
-    console.log(data);
-
     return data;
   } catch (error) {
     throw error;
