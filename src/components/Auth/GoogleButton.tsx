07589c792e7b7d2dd1b254257f59d8c50fe81205
RanjanaRK <ranjanakumari.me@gmail.com>
2026-03-30 23:54:25 +0530

    Update GoogleButton.tsx

--- src/components/Auth/GoogleButton.tsx
+++ src/components/Auth/GoogleButton.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import { useAuth } from "@/hooks/useAuth";
-import { Button } from "../ui/button";
-import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
-import { AuthContext } from "@/context/auth.context";
-import { useContext } from "react";
+import { Button } from "../ui/button";
 
 const GoogleButton = () => {
   const router = useRouter();
 
   const googleHandle = async () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/google`;
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/google`;
   };
 
   return (
