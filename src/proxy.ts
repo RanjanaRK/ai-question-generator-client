1ff9216e58d310ffb8700168a8c606b8a1a372bb
RanjanaRK <ranjanakumari.me@gmail.com>
2026-08-05 00:27:35 +0530

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -10,5 +10,5 @@ export function proxy(request: NextRequest) {
 // export default function proxy(request: NextRequest) { ... }
 
 export const config = {
-  matcher: "/about/:path*",
+  matcher: "/",
 };
