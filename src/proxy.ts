29cf5a1b11c098d01dd7e5f3e9ea09bd93c3b3cb
RanjanaRK <ranjanakumari.me@gmail.com>
2026-08-19 23:09:09 +0530

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,16 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+export function proxy(req: NextRequest) {
+  const accessToken = req.cookies.get("access_token")?.value;
+  const refreshToken = req.cookies.get("refresh_token")?.value;
+
+  if (accessToken || refreshToken) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL("/auth/login", req.url));
 }
 
-// Alternatively, you can use a default export:
-// export default function proxy(request: NextRequest) { ... }
-
 export const config = {
-  matcher: "/",
+  matcher: ["/profile/:path*", "/upgrade-plan/:path*"],
 };
