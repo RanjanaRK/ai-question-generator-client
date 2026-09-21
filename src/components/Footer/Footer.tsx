8707b307bd1ed4b78449fb8c3d28a3d2f39893b0
RanjanaRK <ranjanakumari.me@gmail.com>
2026-09-21 15:53:17 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <>
-      <footer className="bg-background border-t">
-        <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-4 px-6 py-6 md:flex-row">
+      <footer className="bg-background fixed right-0 bottom-0 left-0 z-50 border-t">
+        <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-4 px-6 py-4 md:flex-row">
           <div className="text-muted-foreground text-sm">
             &#169; {new Date().getFullYear()} AI Question Generator. All rights reserved.
           </div>
